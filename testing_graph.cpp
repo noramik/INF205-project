47fcc49b380a550af7c3f2a43565ff180e2a910c
noramik <nora-mik@outlook.com>
2022-11-15 11:48:58 +0100

    Added comments

--- testing_graph.cpp
+++ testing_graph.cpp
@@ -10,9 +10,10 @@ int main()
 	std::string in_label = "is";
 	g->create_node(in_label);
 	//std::vector<Node*> nodes = g->get_nodes();
-	std::cout << g->get_nodes().at(0);
+	std::cout << g->get_nodes().at(0)->get_label();
 
 
-	delete g;
+	//delete g; // Seems like there is something wrong with the destructor. When I comment out this line everything works fine
+	// but when it isn't commented, I get an error message when trying to compile.
 
 }
