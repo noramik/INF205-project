6098d8389019906845d2929b593750c7c3eebd3e
noramik <nora-mik@outlook.com>
2022-11-15 17:37:08 +0100

    Testing if the node map works

--- testing_graph.cpp
+++ testing_graph.cpp
@@ -9,8 +9,11 @@ int main()
 	graph::Graph* g = new graph::Graph;
 	std::string in_label = "is";
 	g->create_node(in_label);
-	//std::vector<Node*> nodes = g->get_nodes();
-	std::cout << g->get_nodes().at(0)->get_label();
+	std::map<std::string, graph::Node*> nodes = g->get_nodes();
+	for (const auto& x: nodes)
+	{
+	std::cout << x.first << x.second << std::endl;
+	}
 
 
 	//delete g; // Seems like there is something wrong with the destructor. When I comment out this line everything works fine
