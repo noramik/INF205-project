370315880511a092a83bd0eddb7de828f440189a
noramik <nora-mik@outlook.com>
2022-11-16 12:02:55 +0100

    Testing reading from file

--- testing_graph.cpp
+++ testing_graph.cpp
@@ -1,9 +1,16 @@
-#include <iostream>
-#include <string>
+//#include <iostream>
+//#include <string>
+
 
 #include "graph.h"
+#include "query.h"
 
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <string>
 using namespace graph;
+/*
 int main()
 {
 	graph::Graph* g = new graph::Graph;
@@ -30,7 +37,37 @@ int main()
 	// but when it isn't commented, I get an error message when trying to compile.
 
 }
+*/
 
+int main(int argc, char** argv)
+{
+   assert(argc >= 3);  //Why is this 3, it throws an error, don't know why
+
+   std::ifstream indata(argv[1]);
+   if(!indata)
+   {
+      std::cerr << "Error! File " << argv[1] << " cannot be read.\n";
+      return EXIT_FAILURE;
+   }
+   graph::Graph g;
+   indata >> g;
+   indata.close();
+   // std::cout << "\nContent of graph g:\n" << g << "\n";
+
+   /*
+   std::ifstream inquery(argv[2]);
+   if(!inquery)
+   {
+      std::cerr << "Error! File " << argv[1] << " cannot be read.\n";
+      return EXIT_FAILURE;
+   }
+   graph::Query q;
+   inquery >> q;
+   inquery.close();
+
+   g.query(&q, &std::cout);  // apply query q to graph g
+   */
+}
 
 
 
