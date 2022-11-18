d1c6dc1807007ffd4d3d5bc23f791c20dbc0c2a2
Hanna Lye Moum <hanna.lye.moum@nmbu.no>
2022-11-18 13:46:55 +0100

    save work. added new function draft for finding final matches

--- draft-alogirthm-2.cpp
+++ draft-alogirthm-2.cpp
@@ -178,18 +178,18 @@ std::tuple<char, *std::vector<Edge*>> analyse_path_edges(string p_label, string 
 }
 
 
-
-//void iterate_forward(Node* node, std::deque<string> stash, int current_index, &var_store) { //COPY stash and index
 void iterate_forward(Edge* edge, std::deque<string> stash, int current_index, &var_store) { //COPY stash and index
     /// iterate forward in the graph
 
 
     if (current_index == var_store.path.size()-1) stash.push_back(edge.get_head_node()); //at the end
 
+    //might remove entirely? change completely
     if (stash.size() == 2) {
-            var_store.found_patterns.push_back(stash);  // a whole path is found! ONLY P
+           // var_store.found_patterns.push_back(stash);  // a whole path is found! ONLY P
                                                         //might change if stash is not a deque...
             //HERE: Start searching for matching q!
+            //and return true if var_store.return_nodes = false and both p and q is found;
     }
 
     //do not continue to iterate if at the end of a path (fully finished path). return to the previous "recursion"
@@ -213,13 +213,13 @@ void iterate_forward(Edge* edge, std::deque<string> stash, int current_index, &v
 }
 
 
-//void iterate_backward(Node* node, const path &path, std::deque<string> stash, int current_index, bool first_iter=false) {// we don't need start_index anymore
 void iterate_backward(Edge* edge, std::deque<string> stash, int current_index, &var_store) {
     //we do not go here if we started at index 0
 
     if (current_index == 0) {
-        stash.push_back(edge.get_tail_node());
-        var_store.found_patterns.push_back(stash); //a whole path found! ONLY p
+        stash.push_front(edge.get_tail_node()); //does not matter which order the nodes are in?? do not use deque...
+        //var_store.found_patterns.push_back(stash); //a whole path found! ONLY p
+         //HERE: Start searching for matching q!
     }
 
     else if (edge.get_tail_node().get_prev_edges()) { // make sure it does not point to a nullpointer
@@ -237,21 +237,64 @@ void iterate_backward(Edge* edge, std::deque<string> stash, int current_index, &
 
         }
     }
-
-
-
 }
+
+//STASH must contain nodes in the order: (start, end)
+void search_match(Node* node, std::deque<string> &stash, int current_index, &var_store) { //match node is the last node in the previously found pattern
+
+    if (current_index == var_store.path.size()-1) {//found an entire path, but does the ending point match?
+
+        if (node == stash.back()) { //.back() has O(1) https://www.geeksforgeeks.org/vectorfront-vectorback-c-stl/
+            //obs! can we compare the pointers? or do we have to compare the labels...
+            var_store.found_patterns.push_back(stash);
+            var_store.match=true //obs! does not work with this copy
+            if (var_store.return_nodes == false) {
+                //..........send to rank 0? but what if only one rank...
+                //return true
+                //else return false?
+            }
+        }
+    }
+
+    else {
+        if (node.get_next_edges()) { //not a nullpointer
+            current_index++;
+            for (Edge* edge: node_get_next_edges()) {
+                if (edge.get_label() == var_store.path[current_index]) {
+                    search_match(edge.get_head_node(), &stash, current_index, &var_store);
+                }
+            }
+        }
+    }
+}
+
 
 // save variables we will use over and over again instead of sending them back and forth between functions. Place in namespace
 struct varStorage{
     //!!innhold av found_patterns må matche med stash som currently er deque
     std::vector<std::vector<Node*>> found_patterns; //start and end nodes connected by both p and q in pairs
     const int start_index; //index from sequence of our starting point
-    Node* start_node; //corresponding tail_node to start_index, where we start iterating. NOT constant, changes in for loops
-    Edge* start_edge; //instead of node. REMOVE START_NODE
+    Edge* start_edge; //instead of node, whwere the traversing starts.
 
     char path_letter; // p or q
-    path path; //actual sequence corresponding to p or q (the one we are searching trough atm)
+    path path; //actual sequence corresponding to p or q (the one we are searching trough atm). Could point to this->p or this->q...
+
+    //idea draft for finding q immediatly... would make a new instance of varStorage for each q we try to find tho....
+    path p;
+    path q;
+
+    void switch_parameters() {
+        if (this->path_letter == "p") {
+            this->path_letter = "q";
+            this->path = this->q;
+        }
+        else {
+            this->path_letter == "p";
+            this->path = this->p;
+        }
+
+    }
+    // would need a copy constructor to transfer the initial parameters!
 
     const bool return_nodes; // user input
     bool match = false; //change to true if p-q match found AND return_nodes=false. Then efficiently exit all recursion
@@ -286,6 +329,8 @@ type T find_pattern(const path p, const path q, bool return_nodes=false) {
     var_store.start_index = start_path_index;
     var_store.path_letter = path_letter;
 
+    var_store.p = p;
+    var_store.q = q;
     var_store.path = (path_letter == "p") ? p : q; //short hand for if-else https://www.w3schools.com/cpp/cpp_conditions_shorthand.asp
     var_store.return_nodes = return_nodes;
 
